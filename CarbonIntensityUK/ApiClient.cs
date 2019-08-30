3f90c435ea821dbfef477929e0834bbae3fbf875
Jordan Rowles <jordansrowles@gmail.com>
2019-08-30 16:35:32 +0100

    remove and sort using

--- CarbonIntensityUK/ApiClient.cs
+++ CarbonIntensityUK/ApiClient.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Net;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace CarbonIntensityUK
 {
--- CarbonIntensityUK/NationalStatistics/NationalStatistics.cs
+++ CarbonIntensityUK/NationalStatistics/NationalStatistics.cs
@@ -1,8 +1,5 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CarbonIntensityUK.NationalStatistics
