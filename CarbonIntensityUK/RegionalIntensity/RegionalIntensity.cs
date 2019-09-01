32d0c4276a400276b6ab1d738c7245d697447ab0
Jordan Rowles <jordansrowles@gmail.com>
2019-09-01 18:38:46 +0100

    covered all of the regional stuff

--- CarbonIntensityUK/RegionalIntensity/RegionalIntensity.cs
+++ CarbonIntensityUK/RegionalIntensity/RegionalIntensity.cs
@@ -7,30 +7,54 @@ namespace CarbonIntensityUK.RegionalIntensity
 {
     public static class RegionalIntensity
     {
+        /// <summary>
+        ///     GET
+        ///         /regional
+        /// </summary>
+        /// <returns>RegionalFromTo schema</returns>
         public static async Task<List<RegionalFromToIntensityReponse>> Get()
         {
             var json = await ApiClient.QueryAsync("https://api.carbonintensity.org.uk/regional");
             return ApiClient.AttemptConvert<List<RegionalFromToIntensityReponse>>(json);
         }
 
+        /// <summary>
+        ///     GET
+        ///         /regional/postcode/{postcode}
+        /// </summary>
+        /// <param name="postcode">Outward code only</param>
+        /// <returns>RegionalId schema</returns>
         public static async Task<List<RegionalIdIntensityResponse>> Get(string postcode)
         {
             var json = await ApiClient.QueryAsync($"https://api.carbonintensity.org.uk/regional/postcode/{postcode}");
             return ApiClient.AttemptConvert<List<RegionalIdIntensityResponse>>(json);
         }
 
+        /// <summary>
+        ///     GET
+        ///         /regional/england
+        ///         /regional/scotland
+        ///         /regional/wales
+        /// </summary>
+        /// <param name="country"></param>
+        /// <returns></returns>
         public static async Task<List<RegionalFromToIntensityReponse>> Get(Country country)
         {
             var json = await ApiClient.QueryAsync($"https://api.carbonintensity.org.uk/regional/{country.ToString().ToLower()}");
             return ApiClient.AttemptConvert<List<RegionalFromToIntensityReponse>>(json);
         }
 
+        /// <summary>
+        ///     GET
+        ///         /regional/regionid/{regionid}
+        /// </summary>
+        /// <param name="regionId"></param>
+        /// <returns></returns>
         public static async Task<List<RegionalIdIntensityResponse>> Get(RegionId regionId)
         {
             var json = await ApiClient.QueryAsync($"https://api.carbonintensity.org.uk/regional/regionid/{(int)regionId}");
             return ApiClient.AttemptConvert<List<RegionalIdIntensityResponse>>(json);
         }
-
 
         /// <summary>
         ///     GET
@@ -57,10 +81,10 @@ namespace CarbonIntensityUK.RegionalIntensity
         /// <param name="from">ISO 8601 string</param>
         /// <param name="postcode">Outward code only</param>
         /// <returns>RegionalId schema</returns>
-        public static async Task<List<RegionalIdIntensityResponse>> Get(IntensityUriOption option, DateTime from, string postcode)
+        public static async Task<RegionalIdIntensityResponse> Get(IntensityUriOption option, DateTime from, string postcode)
         {
-            var json = await ApiClient.QueryAsync($"https://api.carbonintensity.org.uk/regional/itensity/{ApiClient.FormatDateTime(from)}/{option.ToString()}/postcode/{postcode}");
-            return ApiClient.AttemptConvert<List<RegionalIdIntensityResponse>>(json);
+            var json = await ApiClient.QueryAsync($"https://api.carbonintensity.org.uk/regional/intensity/{ApiClient.FormatDateTime(from)}/{option.ToString()}/postcode/{postcode}");
+            return ApiClient.AttemptConvert<RegionalIdIntensityResponse>(json);
         }
 
         /// <summary>
@@ -73,10 +97,10 @@ namespace CarbonIntensityUK.RegionalIntensity
         /// <param name="from">ISO 8601 string</param>
         /// <param name="region">Region ID</param>
         /// <returns>RegionalId schema</returns>
-        public static async Task<List<RegionalIdIntensityResponse>> Get(IntensityUriOption option, DateTime from, RegionId region)
+        public static async Task<RegionalIdIntensityResponse> Get(IntensityUriOption option, DateTime from, RegionId region)
         {
-            var json = await ApiClient.QueryAsync($"https://api.carbonintensity.org.uk/regional/itensity/{ApiClient.FormatDateTime(from)}/{option.ToString()}/regionid/{(int)region}");
-            return ApiClient.AttemptConvert<List<RegionalIdIntensityResponse>>(json);
+            var json = await ApiClient.QueryAsync($"https://api.carbonintensity.org.uk/regional/intensity/{ApiClient.FormatDateTime(from)}/{option.ToString()}/regionid/{(int)region}");
+            return ApiClient.AttemptConvert<RegionalIdIntensityResponse>(json);
         }
 
         /// <summary>
@@ -100,10 +124,10 @@ namespace CarbonIntensityUK.RegionalIntensity
         /// <param name="to">ISO 8601 string</param>
         /// <param name="postcode">Outward postcode only</param>
         /// <returns>RegionalId schema</returns>
-        public static async Task<List<RegionalIdIntensityResponse>> Get(DateTime from, DateTime to, string postcode)
+        public static async Task<RegionalIdIntensityResponse> Get(DateTime from, DateTime to, string postcode)
         {
-            var json = await ApiClient.QueryAsync($"https://api.carbonintensity.org.uk/regional/intensity/{ApiClient.FormatDateTime(from)}/{ApiClient.FormatDateTime(to)}/regionid/{postcode}");
-            return ApiClient.AttemptConvert<List<RegionalIdIntensityResponse>>(json);
+            var json = await ApiClient.QueryAsync($"https://api.carbonintensity.org.uk/regional/intensity/{ApiClient.FormatDateTime(from)}/{ApiClient.FormatDateTime(to)}/postcode/{postcode}");
+            return ApiClient.AttemptConvert<RegionalIdIntensityResponse>(json);
         }
 
         /// <summary>
@@ -114,10 +138,10 @@ namespace CarbonIntensityUK.RegionalIntensity
         /// <param name="to">ISO 8601 string</param>
         /// <param name="regionId">Region ID</param>
         /// <returns>RegionalId schema</returns>
-        public static async Task<List<RegionalIdIntensityResponse>> Get(DateTime from, DateTime to, RegionId regionId)
+        public static async Task<RegionalIdIntensityResponse> Get(DateTime from, DateTime to, RegionId regionId)
         {
             var json = await ApiClient.QueryAsync($"https://api.carbonintensity.org.uk/regional/intensity/{ApiClient.FormatDateTime(from)}/{ApiClient.FormatDateTime(to)}/regionid/{(int)regionId}");
-            return ApiClient.AttemptConvert<List<RegionalIdIntensityResponse>>(json);
+            return ApiClient.AttemptConvert<RegionalIdIntensityResponse>(json);
         }
 
     }
